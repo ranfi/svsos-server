fd736b59f91507ed11d8b6eee633b82d83e3d408
ranfi <renhefeng@gmail.com>
2014-11-18 22:42:39 +0800

    提交未完成工单controller

---
+++ svsos-web/src/main/java/com/svsos/backend/controller/wx/UoDoOrderController.java
@@ -0,0 +1,65 @@
+package com.svsos.backend.controller.wx;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.svsos.backend.model.ServiceOrder;
+import com.svsos.backend.model.WorkUser;
+import com.svsos.backend.repositories.jpa.ServiceOrderDao;
+import com.svsos.backend.repositories.jpa.WorkUserDao;
+import com.svsos.backend.service.CommonService;
+import com.svsos.backend.weixin.util.WeixinUtil;
+/** 
+ * 待完成工单
+ * @author zhouliangjun 
+ * @date 2014-11-15 
+ */ 
+@Controller
+@RequestMapping("/wx/unorder")
+public class UoDoOrderController {
+
+	@Resource 
+    private WorkUserDao workUserDao;
+	
+	@Resource
+	private CommonService commonService;
+	
+	@Resource ServiceOrderDao serviceOrderDao;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Response response , Request request ,Model model) {
+		List<ServiceOrder> orderLists = new ArrayList<ServiceOrder>(); 
+	    String username = WeixinUtil.getCookieValue(request);
+	    if(username != null){	    	
+	        WorkUser user = workUserDao.findWorkUserByAccount(username);
+	        if(user != null){	            	
+            	List<ServiceOrder> res = serviceOrderDao.findServiceOrderByacceptWorkerIdAndStatus(user.getId());
+            	if(null != res && res.size() > 0)
+    			{
+    				for(int i = 0;i < res.size(); i ++)
+    				{
+    					ServiceOrder orderlist = new ServiceOrder();
+    					orderlist.setOrderLsh(res.get(i).getOrderLsh());
+    					orderlist.setCreateTime(res.get(i).getCreateTime());
+    					orderlist.setStatus(res.get(i).getStatus());
+    					orderlist.setName(res.get(i).getName());
+    					orderlist.setTel(res.get(i).getTel());
+    					orderlist.setAddress(res.get(i).getAddress());
+    					orderlist.setProductBrand(res.get(i).getProductBrand());    					
+    					orderLists.add(orderlist);
+    				}
+    			}
+            }            	
+        }
+		model.addAttribute("orderLists", orderLists);
+		return "weixin/undoorder";
+	}
+}
